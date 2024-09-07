12a58f853ef789460297eb9dfc47111129223a39
j-chcode <berkchcode@gmail.com>
2024-09-07 13:03:02 +0300

    Create retime.js

---
+++ api/retime.js
@@ -0,0 +1,22 @@
+const retime = (data) => {
+	const targetTime = data;
+
+  if (!targetTime) {
+    return res.status(400).json({ error: "Lütfen bir saat formatı girin. Örnek: '23:00'" });
+  }
+
+  const now = new Date();
+  const [hours, minutes] = targetTime.split(':');
+  const targetDateTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
+
+  if (targetDateTime < now) {
+    targetDateTime.setDate(targetDateTime.getDate() + 1);
+  }
+
+  const timeDifference = targetDateTime - now;
+  const remainingSeconds = Math.floor(timeDifference / 1000);
+
+  return { remainingSeconds }
+}
+
+module.exports = retime;
