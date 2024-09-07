264604dec5b3a1b5212d8a027ab54bc03ab3a50b
j-chcode <berkchcode@gmail.com>
2024-09-07 14:52:39 +0300

    Create anime.js

---
+++ api/anime.js
@@ -0,0 +1,25 @@
+const anime = (type, tag) => {
+  const axios = require('axios');
+  
+	try {
+
+    const response = await axios.get(`https://api.waifu.pics/${type}/${tag}`);
+    const imageUrl = response.data.url;
+
+    return {
+      status: 200,
+      imageUrl
+    }
+
+  } catch (error) {
+
+    return {
+      status: 400,
+      error: "Bir hata oluştu. Lütfen tekrar deneyin."
+      string: error.message
+    }
+
+  }
+}
+
+module.exports = anime;
