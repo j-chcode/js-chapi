89210d6f09f0f2ad4178c302e191ec8afc47f145
j-chcode <berkchcode@gmail.com>
2024-09-07 15:43:53 +0300

    Update anime.js

--- api/anime.js
+++ api/anime.js
@@ -1,24 +1,20 @@
 const anime = async (type, tag) => {
   const axios = require('axios');
   
-	try {
-
+  try {
     const response = await axios.get(`https://api.waifu.pics/${type}/${tag}`);
     const imageUrl = response.data.url;
-
     return {
       status: 200,
       imageUrl
     }
-
-  } catch (error) {
-
+  }
+  catch (error) {
     return {
       status: 400,
       error: "Bir hata oluştu. Lütfen tekrar deneyin."
       string: error.message
     }
-
   }
 }
 
