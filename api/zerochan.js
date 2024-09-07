aa28b40c994bdf5bc4aff426f5dd84cb9cc4b9c5
j-chcode <berkchcode@gmail.com>
2024-09-07 18:19:50 +0300

    Update zerochan.js

--- api/zerochan.js
+++ api/zerochan.js
@@ -1,32 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const zerochan = async () => {
-  const axios = require('axios');
-  const cheerio = require('cheerio');
-  
-  const url = 'https://www.zerochan.net';
-  
+  const url = 'https://www.zerochan.net/Horo';
+
   async function fetchImages() {
     try {
-      const { data } = await axios.get(url);
+      const { data } = await axios.get(url, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+      });
+
       const $ = cheerio.load(data);
       let imageUrls = [];
 
       $('img').each((index, element) => {
         const imgUrl = $(element).attr('src');
-        if (imgUrl) {
+        if (imgUrl && imgUrl.includes('static.zerochan.net') && imgUrl.includes('full')) {
           imageUrls.push(imgUrl);
         }
       });
-      
-    return imageUrls;
+
+      return imageUrls;
     } catch (error) {
       console.error('Hata:', error);
+      return [];
     }
   }
 
   const data = await fetchImages();
-  return {
-    data
-  }
+  return { data };
 }
 
 module.exports = zerochan;
