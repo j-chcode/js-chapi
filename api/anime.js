a2c50d4907ae80a99f867ed76972c4ea36e4987b
j-chcode <berkchcode@gmail.com>
2024-09-07 17:38:36 +0300

    Update anime.js

--- api/anime.js
+++ api/anime.js
@@ -1,9 +1,16 @@
-const anime = async (type, tag) => {
+const anime = async (type, tag, amount) => {
   const axios = require('axios');
   
   try {
-    const response = await axios.get(`https://api.waifu.pics/${type}/${tag}`);
-    const imageUrl = response.data.url;
+    let imageUrl = [];
+    let len = 0;
+    
+    for(let i = 0; i < amount; i++) {
+      const response = await axios.get(`https://api.waifu.pics/${type}/${tag}`)
+      imageUrl[(len +1)] = response.data.url;
+      len += imageUrl.length;
+    }
+
     return {
       status: 200,
       imageUrl
