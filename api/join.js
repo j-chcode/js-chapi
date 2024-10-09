001e1235e813b08a340f5f8ae543fb0db688c162
j-chcode <berkchcode@gmail.com>
2024-10-10 01:21:53 +0300

    Update join.js

--- api/join.js
+++ api/join.js
@@ -1,32 +1,34 @@
 const join = async (token, channelId) => {
     const { Client, GatewayIntentBits } = require('discord.js');
+    const { joinVoiceChannel } = require('@discordjs/voice');
     
+    const { token, channelId } = req.body;
+
     if (!token || !channelId) {
-        return { error: 'Token ve Kanal ID\'si gereklidir' }
+        return { error: 'Token ve Kanal ID\'si gereklidir' };
     }
 
-    const client = new Client({
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildVoiceStates, // Sesli kanalları izlemek için gerekli intent
-        ]
-    });
-    
     try {
         // Botu token ile giriş yapalım
         await client.login(token);
 
-        // Ses kanalına katılalım
+        // Ses kanalını bulalım
         const channel = await client.channels.fetch(channelId);
         if (channel && channel.isVoiceBased()) {
-            await channel.join();
-            return { message: 'Bot ses kanalına katıldı!' }
+            // Ses kanalına katılalım
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+                selfDeaf: false, // Botun kendini susturup susturmayacağı
+            });
+            return { message: 'Bot ses kanalına katıldı!' };
         } else {
-            return { error: 'Geçersiz kanal ID\'si veya kanal ses kanalı değil.' }
+            return { error: 'Geçersiz kanal ID\'si veya kanal ses kanalı değil.' };
         }
     } catch (error) {
-        console.error(error);
-        return { error: 'Bot ses kanalına katılamadı.' }
+        console.error('Bot hata verdi:', error);
+        return { error: 'Bot ses kanalına katılamadı.' };
     }
 };
 
